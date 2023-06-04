41ef6c779bc52a3579ec60e0b51ebefe680b0dd4
matueszD <mati.d@gmail.com>
2023-06-04 15:35:23 +0200

    Zadanie ukończone

--- src/com/company/EmployeeManager.java
+++ src/com/company/EmployeeManager.java
@@ -1,60 +1,78 @@
 package com.company;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class EmployeeManager {
+    private final HashMap<Integer, Employee> employees;
 
-    private final HashMap<Integer,Employee> employees;
+    public HashMap<Integer, Employee> getEmployees() {
+        return this.employees;
+    }
 
     public EmployeeManager(HashMap<Integer, Employee> employees) {
         this.employees = employees;
     }
 
-    public  void addEmployee(Employee employee){
+    public void addEmployee(String firstName, String lastName, String position, int salary) {
+        Employee employee = new Employee(firstName, lastName, position, salary);
         Integer keyEmployee = employee.getEmployeeID();
-        employees.put(keyEmployee,employee);
-    }
-    public void removeEmployee(Employee employee){
-        if(employees.containsKey(employee.getEmployeeID()))
-            employees.remove(employee.getEmployeeID());
-        else
-            System.out.println("Pracownik o takim ID nie istnieje");
+        this.employees.put(keyEmployee, employee);
     }
 
-    public void updateEmployee(int employeeID, Employee newEmployee){
-        if(employees.containsKey(employeeID))
-        {
-            employees.put(employeeID,newEmployee);
-        }
-        else
+    public void removeEmployee(Employee employee) {
+        if (this.employees.containsKey(employee.getEmployeeID())) {
+            this.employees.remove(employee.getEmployeeID());
+        } else {
             System.out.println("Pracownik o takim ID nie istnieje");
-    }
-    public Employee searchEmployeeByID(int employeeID) {
-        if(employees.containsKey(employeeID)){
-            return employees.get(employeeID);
         }
-        else
-            System.out.println("Nie znaleziono pracownika o danym ID");
-           return null;
+
     }
-    public void listAllEmployees(){
-        ArrayList<Employee> employeeList = new ArrayList<>(employees.values());
-        for (Employee employee: employeeList
-             ) {
+
+    public void updateEmployee(int employeeID, String newPosition, int newSalary) {
+        if (searchEmployeeByID(employeeID) != null){
+            Employee e = searchEmployeeByID(employeeID);
+            e.setPosition(newPosition);
+            e.setSalary(newSalary);
+        } else {
+            System.out.println("Pracownik o takim ID nie istnieje");
+        }
+
+    }
+
+    public Employee searchEmployeeByID(int employeeID) {
+        if (this.employees.containsKey(employeeID)) {
+            return (Employee)this.employees.get(employeeID);
+        } else {
+            System.out.println("Nie znaleziono pracownika o danym ID");
+            return null;
+        }
+    }
+
+    public void listAllEmployees() {
+        ArrayList<Employee> employeeList = new ArrayList(this.employees.values());
+        Iterator var2 = employeeList.iterator();
+
+        while(var2.hasNext()) {
+            Employee employee = (Employee)var2.next();
             System.out.println(employee);
         }
+
     }
-    public HashSet<Employee> searchEmployeesByPosition(String position){
-         HashSet<Employee> employeeList = new HashSet<>(employees.values());
+
+    public HashSet<Employee> searchEmployeesByLastName(String lastName) {
+        HashSet<Employee> employeeList = new HashSet(this.employees.values());
         Iterator<Employee> employeeIterator = employeeList.iterator();
-        while(employeeIterator.hasNext())
-        {
-            Employee employee = employeeIterator.next();
-            if(!employee.getPosition().equals(position))
+
+        while(employeeIterator.hasNext()) {
+            Employee employee = (Employee)employeeIterator.next();
+            if (!employee.getLastName().equals(lastName)) {
                 employeeIterator.remove();
+            }
         }
+
         return employeeList;
     }
-
-
 }
